e0c5d4880fabf5891e1709b36a84a1de9c636f1a
josealonso <jose.alonso.programmer@gmail.com>
2022-05-25 01:02:11 +0200

    Added library to convert our SVG and JSON to Base64 in Solidity

--- contracts/MyRandomEpicNFT.sol
+++ contracts/MyRandomEpicNFT.sol
@@ -13,43 +13,83 @@ contract MyRandomEpicNFT is ERC721URIStorage {
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
 
-// This is our SVG code. All we need to change is the word that's displayed. Everything else stays the same.
-  // So, we make a baseSvg variable here that all our NFTs can use.
-  string baseSvg = "<svg xmlns='http://www.w3.org/2000/svg' preserveAspectRatio='xMinYMin meet' viewBox='0 0 350 350'><style>.base { fill: white; font-family: serif; font-size: 24px; }</style><rect width='100%' height='100%' fill='black' /><text x='50%' y='50%' class='base' dominant-baseline='middle' text-anchor='middle'>";
+    // This is our SVG code. All we need to change is the word that's displayed. Everything else stays the same.
+    // So, we make a baseSvg variable here that all our NFTs can use.
+    string baseSvg =
+        "<svg xmlns='http://www.w3.org/2000/svg' preserveAspectRatio='xMinYMin meet' viewBox='0 0 350 350'><style>.base { fill: white; font-family: serif; font-size: 24px; }</style><rect width='100%' height='100%' fill='black' /><text x='50%' y='50%' class='base' dominant-baseline='middle' text-anchor='middle'>";
 
-  // I create three arrays, each with their own theme of random words.
-  // Pick some random funny words, names of anime characters, foods you like, whatever! 
-  string[] firstWords = ["Alto", "Bajo", "Tonto", "Listo", "Elegante", "Presumido"];
-  string[] secondWords = ["Caballero", "Animal", "Perro", "Gato", "Lobo", "Cangrejo"];
-  string[] thirdWords = ["Blanco", "Amarillo", "Azul", "Negro", "Rojo", "Gris"];
+    // I create three arrays, each with their own theme of random words.
+    // Pick some random funny words, names of anime characters, foods you like, whatever!
+    string[] firstWords = [
+        "Alto",
+        "Bajo",
+        "Tonto",
+        "Listo",
+        "Elegante",
+        "Presumido"
+    ];
+    string[] secondWords = [
+        "Caballero",
+        "Animal",
+        "Perro",
+        "Gato",
+        "Lobo",
+        "Cangrejo"
+    ];
+    string[] thirdWords = [
+        "Blanco",
+        "Amarillo",
+        "Azul",
+        "Negro",
+        "Rojo",
+        "Gris"
+    ];
 
     constructor() ERC721("SquareNFT", "SQUARE") {
         console.log("This is my NFT contract. Whoa!");
     }
 
-// I create a function to randomly pick a word from each array.
-  function pickRandomFirstWord(uint256 tokenId) public view returns (string memory) {
-    uint256 rand = random(string(abi.encodePacked("FIRST_WORD", Strings.toString(tokenId))));
-    // Squash the # between 0 and the length of the array to avoid going out of bounds.
-    rand = rand % firstWords.length;
-    return firstWords[rand];
-  }
+    // I create a function to randomly pick a word from each array.
+    function pickRandomFirstWord(uint256 tokenId)
+        public
+        view
+        returns (string memory)
+    {
+        uint256 rand = random(
+            string(abi.encodePacked("FIRST_WORD", Strings.toString(tokenId)))
+        );
+        // Squash the # between 0 and the length of the array to avoid going out of bounds.
+        rand = rand % firstWords.length;
+        return firstWords[rand];
+    }
 
-  function pickRandomSecondWord(uint256 tokenId) public view returns (string memory) {
-    uint256 rand = random(string(abi.encodePacked("SECOND_WORD", Strings.toString(tokenId))));
-    rand = rand % secondWords.length;
-    return secondWords[rand];
-  }
+    function pickRandomSecondWord(uint256 tokenId)
+        public
+        view
+        returns (string memory)
+    {
+        uint256 rand = random(
+            string(abi.encodePacked("SECOND_WORD", Strings.toString(tokenId)))
+        );
+        rand = rand % secondWords.length;
+        return secondWords[rand];
+    }
 
-  function pickRandomThirdWord(uint256 tokenId) public view returns (string memory) {
-    uint256 rand = random(string(abi.encodePacked("THIRD_WORD", Strings.toString(tokenId))));
-    rand = rand % thirdWords.length;
-    return thirdWords[rand];
-  }
+    function pickRandomThirdWord(uint256 tokenId)
+        public
+        view
+        returns (string memory)
+    {
+        uint256 rand = random(
+            string(abi.encodePacked("THIRD_WORD", Strings.toString(tokenId)))
+        );
+        rand = rand % thirdWords.length;
+        return thirdWords[rand];
+    }
 
-  function random(string memory input) internal pure returns (uint256) {
-      return uint256(keccak256(abi.encodePacked(input)));
-  }
+    function random(string memory input) internal pure returns (uint256) {
+        return uint256(keccak256(abi.encodePacked(input)));
+    }
 
     function makeAnEpicNFT() public {
         uint256 newItemId = _tokenIds.current();
@@ -58,12 +98,13 @@ contract MyRandomEpicNFT is ERC721URIStorage {
         string memory second = pickRandomSecondWord(newItemId);
         string memory third = pickRandomThirdWord(newItemId);
 
-
-        string memory finalSvg = string(abi.encodePacked(baseSvg, first, second, third, "</text></svg>"));        
+        string memory finalSvg = string(
+            abi.encodePacked(baseSvg, first, second, third, "</text></svg>")
+        );
         console.log("\n--------------------");
         console.log(finalSvg);
         console.log("--------------------\n");
-        
+
         _safeMint(msg.sender, newItemId);
 
         // Set the NFTs data.
@@ -75,6 +116,5 @@ contract MyRandomEpicNFT is ERC721URIStorage {
             newItemId,
             msg.sender
         );
-
     }
 }
